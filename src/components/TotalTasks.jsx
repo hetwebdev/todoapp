67223e1ef0770601b4c410fc8746ebe4eac6a5b2
HETPATEL <het369patel@gmail.com>
2025-01-13 23:16:21 +0530

    Modification TotalTasks.jsx
    Delete Button Modification

--- src/components/TotalTasks.jsx
+++ src/components/TotalTasks.jsx
@@ -9,9 +9,14 @@ const TotalTasks = ({ tasks, onDeleteAllTask }) => {
       <div className="font-inter font-medium text-slate-700 px-4 py-2 bg-slate-300 rounded-sm">
         {tasks.length || 0}
       </div>
-      <div className="text-white px-4 py-2 bg-red-500 rounded-sm cursor-pointer hover:bg-red-700">
+      <button
+        disabled={tasks.length === 0 ? true : false}
+        className={`text-white px-4 py-2  rounded-sm cursor-pointer  ${
+          tasks.length === 0 ? "bg-red-300" : "bg-red-500 hover:bg-red-700"
+        }`}
+      >
         <RiDeleteBinLine className="size-6" onClick={onDeleteAllTask} />
-      </div>
+      </button>
     </div>
   );
 };
